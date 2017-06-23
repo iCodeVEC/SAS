17209a544ce0113e4609d6f25780fc346f9d1702
Saran Balaji <csaranbalaji@gmail.com>
2017-06-23 19:49:53 +0530

    trim login php module

--- icodelogin.php
+++ icodelogin.php
@@ -1,47 +1,4 @@
-<html>
-    <head>
-      <title>Icode Login</title>
-	  <meta name="viewport" content="width=device-width, initial-scale=1">      
-	  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
-      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>           
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"></script> 
-    </head>
-
-    <body>
-	<div class="row">
-		<div class="col s12 m8 offset-m2 l4 offset-l4" style="padding-top: 6%;">
-        <div class="card-panel grey lighten-5 z-depth-1">
-			<h4 class="header2" style="vertical-align:center;">iCode Login</h4>
-			<form action="‪icodelogin.php" method="post">
-			  <div class="row">
-				<div class="input-field col s12">
-				  <i class="material-icons prefix">email</i>
-				  <input id="id" type="text" class="validate" name="Pharmacy-id" autofocus required>
-				  <label for="id">Admin ID</label>
-				</div>
-			  </div>
-			  <div class="row">
-				<div class="input-field col s12">
-				  <i class="material-icons prefix">lock</i>
-				  <input id="password" type="password" name="Password" class="validate" required>
-				  <label for="password">Password</label>
-				</div>
-
-				<div class="input-field col s12">
-					<button class="btn waves-effect waves-light right submit" type="submit" name="action">Login
-					  <i class="mdi-content-send right"></i>
-					</button>
-				</div>
-			  </div>
-			  
-			</form>
-			
-
-			<p>New to us? <a href="pharmacy-reg.html">Sign Up</a></p>
-        </div>
-    </div>
-	  <?php 
+<?php 
 			session_start(); 
 			$dbname="confidential";
 			
@@ -76,8 +33,4 @@
                 }
 			}
  ?>
-      <!--Import jQuery & materialize.js-->
-      <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
+      
