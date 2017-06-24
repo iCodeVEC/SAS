14024423a0e7651125aab5f5a9830f54da8e770d
Saran Balaji <csaranbalaji@gmail.com>
2017-06-24 19:51:02 +0530

    indend login php module

--- icodelogin.php
+++ icodelogin.php
@@ -1,36 +1,35 @@
 <?php 
-			session_start(); 
-			$dbname="confidential";
-			
-			$conn = new mysqli("localhost","root","","$dbname"); 
-			
+	session_start(); 
+	$dbname="confidential";
 	
-			
-			if($_SERVER["REQUEST_METHOD"]== "POST") 
-				
-			{ 
-			
-		     $myusername = $_POST['username'];
-			 
-			 $mypassword = $_POST['password']; 
-      
-		     $sql = "SELECT * FROM table WHERE Email = '$myusername' and Password= '$mypassword'";
-			
-			 $result = mysqli_query($db,$sql) or die("error in inserting data in db" . mysql_error());
-			 
-			 $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-			  
-			 $count = mysqli_num_rows($result);
-			   
-			if($count == 1) 
-		   {
-			   $_SESSION['login_user'] = $myusername;
-			   exit;
-			  }
-	            else 
-	            {
-                 $error = "Your Email or Password is invalid";
-                }
-			}
+	$conn = new mysqli("localhost","root","","$dbname"); 
+	
+
+	
+	if($_SERVER["REQUEST_METHOD"]== "POST") 
+		
+	{ 
+	
+	 $myusername = $_POST['username'];
+	 
+	 $mypassword = $_POST['password']; 
+
+	 $sql = "SELECT * FROM table WHERE Email = '$myusername' and Password= '$mypassword'";
+	
+	 $result = mysqli_query($db,$sql) or die("error in inserting data in db" . mysql_error());
+	 
+	 $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+	  
+	 $count = mysqli_num_rows($result);
+	   
+	if($count == 1) 
+   {
+	   $_SESSION['login_user'] = $myusername;
+	   exit;
+	  }
+		else 
+		{
+		 $error = "Your Email or Password is invalid";
+		}
+	}
  ?>
-      
