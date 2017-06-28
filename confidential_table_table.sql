d95f0a1464a93e33a6b912e5defadcc9cfa81682
Saran Balaji <csaranbalaji@gmail.com>
2017-06-28 18:36:56 +0530

    Database added

---
+++ confidential_table_table.sql
@@ -0,0 +1,18 @@
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table`
+--
+
+CREATE TABLE `table` (
+  `Email` varchar(40) NOT NULL,
+  `Password` varchar(40) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `table`
+--
+
+INSERT INTO `table` (`Email`, `Password`) VALUES
+('kushalkataria5@gmail.com', 'kushalchand');
