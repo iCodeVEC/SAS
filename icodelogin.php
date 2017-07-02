2892f4e9b8a299a800f9de81fdfb4252d97ec392
iamkkushal <kushalkataria5@gmail.com>
2017-07-02 17:31:36 +0530

    Updated

--- icodelogin.php
+++ icodelogin.php
@@ -1,35 +1,26 @@
 <?php 
 	session_start(); 
-	$dbname="confidential";
 	
+	$dbname="personal";
 	$conn = new mysqli("localhost","root","","$dbname"); 
 	
-
-	
 	if($_SERVER["REQUEST_METHOD"]== "POST") 
-		
 	{ 
-	
-	 $myusername = $_POST['username'];
-	 
-	 $mypassword = $_POST['password']; 
-
-	 $sql = "SELECT * FROM table WHERE Email = '$myusername' and Password= '$mypassword'";
-	
-	 $result = mysqli_query($db,$sql) or die("error in inserting data in db" . mysql_error());
-	 
+	 $myusername = $_POST['iCode-id'];
+	 $mypassword = $_POST['Password']; 
+	 $sql = "SELECT * FROM confidential WHERE Email = '$myusername' and Password= '$mypassword'";
+	 $result = mysqli_query($conn,$sql) or die("error in inserting data in db" . mysql_error());
 	 $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-	  
 	 $count = mysqli_num_rows($result);
 	   
-	if($count == 1) 
+	 if($count == 1) 
    {
-	   $_SESSION['login_user'] = $myusername;
+	   echo "Success";
 	   exit;
 	  }
 		else 
 		{
-		 $error = "Your Email or Password is invalid";
+		 echo "Your Email or Password is invalid";
 		}
 	}
  ?>
