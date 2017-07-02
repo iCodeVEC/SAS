64a37382d527c4ea1a62d833539de7648710eff6
iamkkushal <kushalkataria5@gmail.com>
2017-07-02 17:37:30 +0530

    Updated the DB

--- confidential_table_table.sql
+++ confidential_table_table.sql
@@ -5,7 +5,7 @@
 -- Table structure for table `table`
 --
 
-CREATE TABLE `table` (
+CREATE TABLE `confidential` (
   `Email` varchar(40) NOT NULL,
   `Password` varchar(40) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -14,5 +14,5 @@ CREATE TABLE `table` (
 -- Dumping data for table `table`
 --
 
-INSERT INTO `table` (`Email`, `Password`) VALUES
+INSERT INTO `confidential` (`Email`, `Password`) VALUES
 ('kushalkataria5@gmail.com', 'kushalchand');
